1821050e3621f6ecd9b19d70202c35bcba6bd42d
sohaib <waytosohaib@gmail.com>
2024-01-22 12:34:32 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+// backend/index.js
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser'); // For parsing JSON in the request body
+const User = require('./models/User');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/qasimdata', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+// Check if the connection is successful
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+  
+    try {
+      // Check if the user exists in the database
+      const user = await User.findOne({ username, password });
+  
+      if (user) {
+        // User exists, send success response
+        res.json({ success: true, message: 'Login successful' });
+      } else {
+        // User not found, send failure response
+        res.json({ success: false, message: 'Invalid username or password' });
+      }
+    } catch (error) {
+      // Handle errors
+      console.error('Error during login:', error);
+      res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+  });
+  
+
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
